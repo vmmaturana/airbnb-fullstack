5d091f86ed02f5dd6b9c8c76cd23a20ff4fbcec7
vmmaturana <vmmaturana@gmail.com>
2022-09-20 16:58:08 +0700

    Created the Models for the different Collections

---
+++ models/houses.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+module.exports = mongoose.model("houses", {
+  description: {
+    type: String,
+    required: true,
+  },
+  host: {
+    type: ObjectId,
+    ref: "users",
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  rooms: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: Text,
+    required: true,
+  },
+});
---
+++ models/reviews.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+module.exports = mongoose.model("reviews", {
+  author: {
+    type: ObjectId,
+    ref: "users",
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  house: {
+    type: ObjectId,
+    ref: "houses",
+    required: true,
+  },
+  rating: {
+    type: Number,
+  },
+});
