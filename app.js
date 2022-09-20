012e01df24b06089483eb9750bb8ee483989ad79
vmmaturana <vmmaturana@gmail.com>
2022-09-20 13:26:14 +0700

    Created the route to the controllers (and somehow a lot of changes duplicated)

--- app.js
+++ app.js
@@ -1,70 +1,71 @@
 // Require Packages
-const createError = require('http-errors')
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const mongoose = require('mongoose')
-const hbs = require('hbs')
-const hbsUtils = require('hbs-utils')(hbs)
-const methodOverride = require('method-override')
-require('dotenv').config()
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const hbs = require("hbs");
+const hbsUtils = require("hbs-utils")(hbs);
+const methodOverride = require("method-override");
+require("dotenv").config();
 
 // Build the App
-const app = express()
+const app = express();
 
 // View Engine (Handlebars)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'hbs')
-app.set('view options', { layout: 'layouts/main' })
-hbs.registerPartials(__dirname + '/views/partials', err => {})
-hbsUtils.registerWatchedPartials(__dirname + '/views/partials')
-require('./hbs-helpers')
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.set("view options", { layout: "layouts/main" });
+hbs.registerPartials(__dirname + "/views/partials", (err) => {});
+hbsUtils.registerWatchedPartials(__dirname + "/views/partials");
+require("./hbs-helpers");
 
 // Middleware
-app.use(logger('tiny'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(logger("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Database
 mongoose.connect(
   process.env.MONGODB_URL,
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
   () => {
-    console.log('Connected to MongoDB')
+    console.log("Connected to MongoDB");
   }
-)
+);
 
 // Security
-require('./express-sessions')(app)
+require("./express-sessions")(app);
 
-// Routes
-
-// ::::
-// Create your routes here
-// ::::
+// Routes - ACA ESCRIBIR LAS RUTAS A LOS CONTROLLERS
+app.use("/auth", require("./controllers/auth"));
+app.use("/bookings", require("./controllers/bookings"));
+app.use("/houses", require("./controllers/houses"));
+app.use("/profile", require("./controllers/profile"));
+app.use("/reviews", require("./controllers/reviews"));
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // Error Handler
 app.use((err, req, res, next) => {
   // Only provides full error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-  res.locals.coder = req.coder
-  res.locals.hideSearch = true
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.coder = req.coder;
+  res.locals.hideSearch = true;
   // Render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+  res.status(err.status || 500);
+  res.render("error");
+});
 
-module.exports = app
+module.exports = app;
